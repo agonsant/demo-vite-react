16f51792f8ddcaba76723b2a44fc81af2950780a
Alex González <alejandrogsa@gmail.com>
2025-01-11 06:51:43 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Demo</h1>
+      <App />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+const parentElement = document.getElementById("root");
+createRoot(parentElement).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>
+);
